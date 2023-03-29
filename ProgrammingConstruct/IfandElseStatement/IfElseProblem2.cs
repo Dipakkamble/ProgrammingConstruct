4d356508fb30d45242806bd395e2fe11348ee02c
Dipakkamble <kambledipak700@gmail.com>
2023-03-29 10:34:34 +0530

    [Dipak] Modified checking even or odd number

---
+++ ProgrammingConstruct/IfandElseStatement/IfElseProblem2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct.IfandElseStatement
+{
+    internal class IfElseProblem2
+    {
+        public static void EvenOdd()
+        {
+            Console.WriteLine("Enter the Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -12,7 +12,10 @@ namespace ProgrammingConstruct
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the programming construct");
+            Console.WriteLine("Checking two numbers equality");
             IfElseProblem1.Equalnum();
+            Console.WriteLine("Checking number is even or odd");
+            IfElseProblem2.EvenOdd();
             Console.ReadLine();
         }
     }
